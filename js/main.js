77b53336cf648174f7f2d59c02bc9da89dbd31a8
secretariats <cecilebourbonnais@gmail.com>
2015-12-14 12:08:55 -0800

    First web commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+function main() {
+
+(function () {
+   'use strict';
+
+ 
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = 80;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+ 
+
+
+}());
+
+
+}
+main();
